76dc44f1ce8d4749afefc9f4cbc3ef8d7e0252db
LeeviJarvinen <t0jale00@students.oamk.fi>
2022-02-20 14:06:44 +0200

    users muutos

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+//GET METODI
+router.get('/eka', function(request,response) {
+  response.send("Hei olen eka get")
+});
+
+//GET METODI PARAMETRILLÄ
+router.get('/eka/:name', function(request,response) {
+  response.send("Hei olen parametri "+request.params.name);
+});
+
+//GET METODI 2 PARAMETRILLÄ
+router.get('/eka/:ename/:sname', function(request,response) {
+  response.send("Hei olen parametri "+request.params.ename+" "+request.params.sname);
+});
+
+//POST METODI
+router.post('/',function(request,response)  {
+    response.send(request.body);
+});
+
 module.exports = router;
